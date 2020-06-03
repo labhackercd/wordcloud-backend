220396209c06919b2fd8e2878328620b6139e4d5
erivanio <erivanio.vanin@gmail.com>
2020-06-03 12:01:15 -0300

    fix path in urls

--- src/wordcloud_backend/urls.py
+++ src/wordcloud_backend/urls.py
@@ -4,13 +4,13 @@ from apps.nlp.views import multigrams_analysis
 from django.conf import settings
 
 if settings.URL_PREFIX:
-    prefix = r'^%s/' % (settings.URL_PREFIX)
+    prefix = '%s/' % (settings.URL_PREFIX)
 else:
-    prefix = r'^'
+    prefix = ''
 
 urlpatterns = [
-    path(prefix + r'', multigrams_analysis),
-    path(prefix + r'admin/', admin.site.urls),
+    path(prefix + '', multigrams_analysis),
+    path(prefix + 'admin/', admin.site.urls),
 ]
 
 admin.site.site_header = 'Wordcloud Backend'
