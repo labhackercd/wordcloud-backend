d35f343eae5ccddde5e2b9a09adce05e0a80c2a1
erivanio <erivanio.vanin@gmail.com>
2020-06-03 10:31:44 -0300

    add force script name

--- src/wordcloud_backend/urls.py
+++ src/wordcloud_backend/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 from apps.nlp.views import multigrams_analysis
+from django.conf import settings
+
+if settings.URL_PREFIX:
+    prefix = r'^%s/' % (settings.URL_PREFIX)
+else:
+    prefix = r'^'
 
 urlpatterns = [
-    path('', multigrams_analysis),
-    path('admin/', admin.site.urls),
+    path(prefix + r'', multigrams_analysis),
+    path(prefix + r'admin/', admin.site.urls),
 ]
+
+admin.site.site_header = 'Wordcloud Backend'
